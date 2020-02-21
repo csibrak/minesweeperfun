594fcdabf918a1838d3f1ce8ea55a95664261796
Tamas Csibrak <tamas.csibrak@gmail.com>
2020-02-21 07:55:14 +0100

    bugfixes and forming code. 'morning commit'

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@
 #include <time.h>
 #include <string.h>
 
-#define PALYAX 23
+#define PALYAX 30
 #define PALYAY 20
+
+#define PALYAMAXX 26
+#define PALYAMAXY 26
 //class minesweeper {
 typedef struct {
 	uint32_t sizex;
@@ -36,8 +39,8 @@ int generate_minefield(t_aknamezo_palya * aknamezo ){
 	int i;
 	for(i=0;i<aknamezo->mines_number;i++){
 		printf("");
-		uint32_t x=(((uint32_t)(rand()))%(aknamezo->sizex-1));
-		uint32_t y=(((uint32_t)(rand()))%(aknamezo->sizey-1));
+		uint32_t x=(((uint32_t)(rand()))%(aknamezo->sizex));
+		uint32_t y=(((uint32_t)(rand()))%(aknamezo->sizey));
 		if(aknamezo->palya[x][y]<255){
 			printf("randomized: mine on x: %d y: %d \r\n",x, y);
 			aknamezo->palya[x][y]=255;  //akna
@@ -47,8 +50,8 @@ int generate_minefield(t_aknamezo_palya * aknamezo ){
 			if((x<aknamezo->sizex-1)&&(y<aknamezo->sizey-1)){aknamezo->palya[x+1][y+1]++;}//bal felső
 			if((x<aknamezo->sizex-1)){aknamezo->palya[x+1][y]++;}//bal
 			if((x>0)&&(y<aknamezo->sizey-1)){aknamezo->palya[x-1][y+1]++;}//jobb felso
-			if((x>0)&&(y>0)){aknamezo->palya[x][y-1]++;}//lent
-			if((x>0)&&(y<aknamezo->sizey-1)){aknamezo->palya[x][y+1]++;}//fent
+			if((y>0)){aknamezo->palya[x][y-1]++;}//lent
+			if((y<aknamezo->sizey-1)){aknamezo->palya[x][y+1]++;}//fent
 		}else{--i;}
 	}
 //	uint32_t maxstep=aknamezo->(sizex*aknamezo->sizey);
@@ -64,10 +67,18 @@ int printminefield(t_aknamezo_palya * mine){
 	for(y=0;y<mine->sizey;y++){
 	for(x=0;x<mine->sizex;x++){
 	if(mine->palya[x][y]<10)
-		{printf("%d ", mine->palya[x][y]  );
+		{if(mine->palya[x][y]<-1){
+		if(mine->palya[x][y]==0){printf(". ");}
+		else{printf("%d ", mine->palya[x][y]+16  );
+		}}else{
+			printf("  ");
+		}
 
-		}else{
-			printf("X ");
+		}
+	else{
+		if(mine->palya[x][y]!=(255-16)){
+			printf("X ");}
+		else{printf("*-");}
 		}
 	}
 	printf("\r\n");
@@ -77,9 +88,10 @@ int printminefield(t_aknamezo_palya * mine){
 
 int step_on_minefield(t_aknamezo_palya * mine, t_step * step){
 	uint32_t val=mine->palya[step->x][step->y];
-	if(val>10){printf("\r\n\n\n\n\n\n\n\n\nBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMMMMMMMMMMMMMM!!");
-	return -1;}
-	else{
+	if(val<0){printf("already revealed %d %d\r\n",step->x,step->y);return 0;}
+	if(val>20){printf("\r\n\n\nBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMMMMMMMMMMMMMM!!");
+	mine->palya[step->x][step->y]-=16;return -1;}
+	else{mine->palya[step->x][step->y]-=16;
 	return 0;
 	}
 }
@@ -87,11 +99,12 @@ int step_on_minefield(t_aknamezo_palya * mine, t_step * step){
 //}
 
 t_aknamezo_palya aknamezo;
+t_aknamezo_palya solver_fieldo;
 int main(int argc, char** argv )
 {
 
 printf("palya size: %d of %d x %d y grid \r\n", sizeof(aknamezo.palya),PALYAX,PALYAY);
-aknamezo.mines_number=10;
+aknamezo.mines_number=(PALYAX*PALYAY /10);
 aknamezo.sizex=PALYAX;
 aknamezo.sizey=PALYAY;
 generate_minefield(&aknamezo);
@@ -99,10 +112,11 @@ t_step step;
 int i=0;
 do{
 step.type=0;step.x=(uint32_t)(rand()%(aknamezo.sizex-1)),step.y=(uint32_t)(rand()%(aknamezo.sizey-1));
+
 printf("\r\nround %d new step on x %d y %d \r\n", ++i,step.x+1,step.y+1);
-printminefield(&aknamezo);
 }
 while(0==step_on_minefield(&aknamezo,&step));
+printminefield(&aknamezo);
 //    waitKey(0);
     return 0;
 }
